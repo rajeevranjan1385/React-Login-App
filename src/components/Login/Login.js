12b6f9e1d2c76f4cebf2f07fca397b881c72b013
rajeevranjan1385 <rajeevranjan1385@gmail.com>
2021-09-22 07:59:04 +0530

    addign cleanup function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('checking key strokes');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEAN UP');
+      clearTimeout(identifier);
+    }
+    
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
